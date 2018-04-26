8eabbcac5b42a5abf6a0b1fd0f3ef8bf6c691296
yonty <Yontymanor@gmail.com>
2018-04-26 15:40:48 +0300

    Creating abbi branch

--- app/src/main/java/com/example/d/pleh/MainActivity.java
+++ app/src/main/java/com/example/d/pleh/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Creating abbu branch
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
