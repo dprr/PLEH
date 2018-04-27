beb7a9aad0dca0ec23520c06947527c7dc9dc9ab
yonty <Yontymanor@gmail.com>
2018-04-27 08:07:30 +0300

    Merge branch 'Abbi' of C:\Users\yonty\AndroidStudioProjects\v1_PLEH with conflicts.

--- app/src/main/java/com/example/d/pleh/WishRequestActivity.java
+++ app/src/main/java/com/example/d/pleh/WishRequestActivity.java
@@ -2,36 +2,67 @@ package com.example.d.pleh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class WishRequestActivity extends AppCompatActivity {
-//    private EditText wishTitle = findViewById(R.id.txtDescPageTitle);
-//    private EditText wishDescription = findViewById(R.id.wish_description);
+    private TextView wishTitle = findViewById(R.id.txtDescPageTitle);
+    private TextView wishDescription = findViewById(R.id.wish_description);
+    private TextView txtDescription = findViewById(R.id.wish_description);
+    private ImageButton btnWishWork = findViewById(R.id.iconWork);
+    private ImageButton btnWishGeneralItems = findViewById(R.id.iconCart);
+    private ImageButton btnWishMed = findViewById(R.id.iconMedicine);
+    private ImageButton btnWishTrans = findViewById(R.id.iconTranslate);
+    private ImageButton btnWishPpl = findViewById(R.id.iconVolunteers);
+    private ImageButton btnRewPiggy = findViewById(R.id.iconPiggyBank);
+    private ImageButton btnRewCookies = findViewById(R.id.iconCookies);
+    private ImageButton btnRewDrink = findViewById(R.id.iconDrink);
+    private ImageButton btnRewFood = findViewById(R.id.iconFood);
+    private ImageButton btnRewGeneralItems = findViewById(R.id.iconGeneral);
+
+    private int btns[] = new int[5];
+    private Button btnRequestWish = findViewById(R.id.btnAskForWish);
+
+    private WishCategoryType wishCategoryType = WishCategoryType.VOLUNTEERS;
+    private RewardCategoryType rewardCategoryType = RewardCategoryType.FOOD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish_request);
 
-//        final Wish wish = new Wish("Beer", RewardCategory.MONEY, "I want a beer", "Eli",
-//                WishCategoryType.VOLUNTEERS, 123);
-//
-//        PlehAPI mAPIService = ApiUtils.getAPIService();
-//        mAPIService.addWish(wish).enqueue(new Callback<Long>() {
-//            @Override
-//            public void onResponse(Call<Long> call, Response<Long> response) {
-//                Long id = response.body();
-//                wish.setId(id);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Long> call, Throwable t) {
-//
-//            }
-//        });
+        btnRequestWish.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Wish wish = new Wish("Feeding Poor", wishCategoryType,
+                        "Help distibuting food", "Spaghetti",
+                        rewardCategoryType, "A loaf of bread", User.getUser().getID());
+
+                PlehAPI mAPIService = ApiUtils.getAPIService();
+
+                mAPIService.addWish(wish).enqueue(new Callback<Long>() {
+                    @Override
+                    public void onResponse(Call<Long> call, Response<Long> response) {
+                        Long id = response.body();
+                        wish.setId(id);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Long> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+
+
     }
 }
